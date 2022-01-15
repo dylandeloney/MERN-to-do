28ef82e505a81defa4b8a0288605ab444edc49fb
Dylan Deloney <dylandeloney@live.com>
2022-01-15 16:54:16 -0600

    made changes to server for production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
+const path = require("path");
 
 const app = express();
 
@@ -25,6 +26,15 @@ app.use("/API/tasks", require("./Routes/API/tasks"));
 app.use("/API/users", require("./Routes/API/users"));
 app.use("/API/auth", require("./Routes/API/auth"));
 app.use("/API/contacts", require("./Routes/API/contacts"));
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
